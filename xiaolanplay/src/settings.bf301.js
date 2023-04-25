b01c25df5ba6d86e617f307cf7609c24be5731bd
AnsWorld <874765637@qq.com>
2023-04-26 01:52:54 +0800

    修改bug

---
+++ xiaolanplay/src/settings.bf301.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",jsList:[],bundleVers:{internal:"52d36",main:"f7e69"}};
