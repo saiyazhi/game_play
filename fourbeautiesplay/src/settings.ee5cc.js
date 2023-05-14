24bb01b36fc250e3cf00883e3eb79b9e13373348
AnsWorld <874765637@qq.com>
2023-05-14 13:20:26 +0800

    听香体验

---
+++ fourbeautiesplay/src/settings.ee5cc.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/StartScene.fire",orientation:"",jsList:[],bundleVers:{internal:"52d36",resources:"ab386",main:"11662"}};
